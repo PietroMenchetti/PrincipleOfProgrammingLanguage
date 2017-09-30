a428364572d6c722ed8d205ffe577c2b68cbf938
Pietro.Menchetti <pietro.menchetti@mail.polimi.it>
2017-09-30 09:42:16 +0200

    let operator

--- ProvaScheme.rkt
+++ ProvaScheme.rkt
@@ -1,4 +1,27 @@
 #lang racket
 
-(+ 2 2)
+(let ((x 1))
+     (let ((y 1))
+  (display (cons x y))
+       (newline)))
 
+(let ((x 1)
+      (y 1))
+  (display (cons x y))
+  (newline))   
+
+(let ((x 1))
+  (let ((x 2))
+      (display x)(newline))
+  (display x)
+  (newline))
+
+(let ((x 2) (y 3))
+  (let* ((x 7)             ;;let* binds sequentially from left to right and the region of a binding is that part of the let* expression to the right of the bindings
+         (z (+ x y)))
+    (* z x)))
+
+(let ((x 2) (y 3))
+  (let ((x 7)
+         (z (+ x y)))
+    (* z x)))
